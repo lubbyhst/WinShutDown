888c452dc2924ea577c442eedd5d3b2807a9478f
Falk <Falk@Bierstube>
2013-01-17 21:51:42 +0100

    Timer comment changed

--- src/javaapplication1/TimerF.java
+++ src/javaapplication1/TimerF.java
@@ -1,6 +1,6 @@
-/*//
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Klasse Timer
+ * 
  */
 package javaapplication1;
 import java.util.Timer;
